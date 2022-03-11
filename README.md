5c32f994ce0abc3aef483bc7a3970dcf2d86ff76
ts-oh <oh.timmms@gmail.com>
2022-03-11 11:21:21 +0700

    finished project, update git repo link

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kanye West Quote Generator
+
+## Description
+
+Created for learning how to use API using JS "Fetch API" https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API. This page generates random Kanye West quotes from Kanye Rest API https://kanye.rest/ when the button is clicked. The page was dynamically generated in JS using factory function.
---
+++ index.js
@@ -0,0 +1,70 @@
+const heading = () => {
+  const title = document.createElement("h2");
+  title.setAttribute("id", "title");
+  title.textContent = `Kanye Rest Quotes`;
+  return title;
+};
+
+const ideaContent = () => {
+  const ideaContainer = document.createElement("div");
+  ideaContainer.setAttribute("id", "idea-container");
+  ideaContainer.textContent = "";
+  return ideaContainer;
+};
+
+const getIdeaButton = () => {
+  const ideaBtn = document.createElement("button");
+  ideaBtn.setAttribute("id", "btn");
+  const ideaBtnFront = document.createElement("span");
+  ideaBtnFront.setAttribute("id", "front");
+  ideaBtnFront.textContent = "CLICK FOR QUOTES";
+  ideaBtn.appendChild(ideaBtnFront);
+  return {
+    ideaBtn,
+    ideaBtnFront,
+  };
+};
+
+const footer = () => {
+  const footerLink = document.createElement("a");
+  footerLink.setAttribute("class", "footer");
+  footerLink.setAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=KauRmlffjqc"
+  );
+  footerLink.textContent = "Inspiration: Build Dumb Shit";
+
+  const footerText = document.createElement("a");
+  footerText.setAttribute("class", "footer");
+  footerText.setAttribute("href", "#");
+  footerText.textContent = "Github Repo";
+
+  return {
+    footerLink,
+    footerText,
+  };
+};
+
+const fetchData = () => {
+  return document.querySelector("#btn").addEventListener("click", function () {
+    fetch("https://api.kanye.rest")
+      .then((response) => response.json())
+      .then((data) => {
+        document.querySelector("#idea-container").innerHTML = `"${data.quote}"`;
+      });
+  });
+};
+
+const mainContainer = () => {
+  const container = document.createElement("div");
+  container.setAttribute("id", "content");
+  document.body.appendChild(container);
+  container.appendChild(heading());
+  container.appendChild(ideaContent());
+  container.appendChild(getIdeaButton().ideaBtn);
+  container.appendChild(footer().footerLink);
+  container.appendChild(footer().footerText);
+  fetchData();
+};
+
+mainContainer();
